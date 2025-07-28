6e194e8aea2853ea45962c2794d81d4aad65e0b2
FeynmanProject <feynmanpro@gmail.com>
2025-07-28 09:17:32 +0800

    Update route.ts

--- app/api/submit-order/route.ts
+++ app/api/submit-order/route.ts
@@ -1,8 +1,7 @@
-// app/api/submit-order/route.ts
 export async function GET(req: Request) {
   try {
     const scriptUrl = "https://script.google.com/macros/s/AKfycbyQR4SAOM-GGQYjcjJ7mfahvsrBQeHH9VfMEfGAI07gLR6zLpgRez31QM9hwVyvd0M/exec";
-    const url = new URL(req.url);
+    const url = new URL(req.url || '');
     const query = url.searchParams.toString();
 
     const response = await fetch(`${scriptUrl}?${query}`, {
@@ -11,6 +10,7 @@ export async function GET(req: Request) {
 
     const text = await response.text();
     return new Response(text, { status: 200 });
+
   } catch (error) {
     console.error('Proxy error:', error);
     return new Response('Proxy failed', { status: 500 });
