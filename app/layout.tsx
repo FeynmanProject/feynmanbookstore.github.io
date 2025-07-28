5024097642b68a29675b5a3cb47bfe2a47c282b5
FeynmanProject <feynmanpro@gmail.com>
2025-07-28 13:52:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
       >
-        <nav className="flex items-center gap-4 px-6 py-4 shadow-md bg-white">
+        <nav className="flex items-center gap-4 px-6 py-4 shadow-md bg-black text-white">
           <Link href="/">
             <div className="flex items-center gap-2">
               <Image
