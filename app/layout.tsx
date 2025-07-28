f512e5aa176676a5c2d10ebf757263b4e466d899
FeynmanProject <feynmanpro@gmail.com>
2025-07-28 13:46:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const pacifico = Pacifico({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-pacifico',
-})
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-pacifico",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +36,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
       >
-        {children}
+        <nav className="flex items-center gap-4 px-6 py-4 shadow-md bg-white">
+          <Link href="/">
+            <div className="flex items-center gap-2">
+              <Image
+                src="/logo-feynmanbookstore.png"
+                alt="Feynman Logo"
+                width={36}
+                height={36}
+              />
+              <span className="text-lg font-semibold">Feynman Bookstore</span>
+            </div>
+          </Link>
+        </nav>
+
+        <main>{children}</main>
       </body>
     </html>
   );
